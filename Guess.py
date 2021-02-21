4af0e9d9d34074fa72f1075911d9c9fc15ca298a
Michał Ogorzałek <michal.ogorzalek@gmail.com>
2021-02-21 17:59:35 +0100

    adding comments

---
+++ Guess.py
@@ -0,0 +1,26 @@
+from random import randint
+import sys
+# generate a number 1~10
+ans = randint(1, 10)
+
+# input from user?
+# check that input is a number 1~10
+def guess_num(guess,answer):
+    try:
+        if 0 < guess < 11:
+            if guess == answer:
+                print('you are a genius!')
+                return True
+            print('try harder')
+            return False
+        else:
+            print('hey bozo, I said 1~10')
+    except ValueError:
+        print('please enter a number')
+        return False
+
+
+if __name__ == "__main__":
+    while True:
+        if (guess_num(int(input('guess a number 1~10:  ')),ans) ): break
+
--- S15Testingexercise.py
+++ S15Testingexercise.py
@@ -1,23 +1,23 @@
 import unittest
 import Guess
 print('token')
-class TestGuess(unittest.TestCase):
+class TestGuess(unittest.TestCase): ##testing if file guess true  when numbers are  equal
     def test_guess_num(self):
         test_param_ans = 10
         test_param_num = 10
         result = Guess.guess_num(test_param_num,test_param_ans)
         self.assertEqual(result, True)
 
-    def test_guess_num2(self):
+    def test_guess_num2(self): ##testing if file guess gives false when numbers are not equal
         test_param_ans = 1
         test_param_num = 10
         result = Guess.guess_num(test_param_num, test_param_ans)
         self.assertEqual(result, False)
-    def test_guess_num3(self):
+    def test_guess_num3(self): ##testing if file guess can take strings
         test_param_ans = 1
         test_param_num = '10'
         result = Guess.guess_num(test_param_num, test_param_ans)
-        self.assertEqual(result, False)
+        self.assertEqual(result, False) #will output error 
 
 unittest.main()
 
